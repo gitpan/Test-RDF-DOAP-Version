df536780a85920bd98193bc9a6aac19ecf7059c6
Toby Inkster <tobyink@cpan.org>
2012-07-05 11:43:59 +0000

    Import of TOBYINK/Test-RDF-DOAP-Version-0.007 from CPAN.
    gitpan-cpan-distribution: Test-RDF-DOAP-Version
    gitpan-cpan-version:      0.007
    gitpan-cpan-path:         TOBYINK/Test-RDF-DOAP-Version-0.007.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- inc/Module/Package/Dist/RDF.pm
+++ inc/Module/Package/Dist/RDF.pm
@@ -1,20 +1,18 @@
 #line 1
 package Module::Package::Dist::RDF;
 
+my $explanation = q<
+	This is the component of Module::Package::RDF which gets
+	bundled with the distribution.
+>;
+
 use 5.008003;
+use base qw[Module::Package::Dist];
+use strict;
+
 BEGIN {
 	$Module::Package::Dist::RDF::AUTHORITY = 'cpan:TOBYINK';
-	$Module::Package::Dist::RDF::VERSION   = '0.005';
-}
-
-package Module::Package::Dist::RDF::standard;
-
-use 5.008003;
-use strict;
-use base qw[Module::Package::Dist];
-BEGIN {
-	$Module::Package::Dist::RDF::standard::AUTHORITY = 'cpan:TOBYINK';
-	$Module::Package::Dist::RDF::standard::VERSION   = '0.005';
+	$Module::Package::Dist::RDF::VERSION   = '0.006';
 }
 
 sub _main
@@ -22,6 +20,17 @@ sub _main
 	my ($self) = @_;
 	$self->mi->trust_meta_yml;
 	$self->mi->auto_install;
+}
+
+{
+	package Module::Package::Dist::RDF::standard;
+	use 5.008003;
+	use strict;
+	BEGIN {
+		$Module::Package::Dist::RDF::standard::AUTHORITY = 'cpan:TOBYINK';
+		$Module::Package::Dist::RDF::standard::VERSION   = '0.006';
+		@Module::Package::Dist::RDF::standard::ISA       = 'Module::Package::Dist::RDF';
+	}
 }
 
 1;
