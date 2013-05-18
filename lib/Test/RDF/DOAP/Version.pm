56c6f504fdfa9e0cf95ab3fbf8c2902079d42b5d
Toby Inkster <tobyink@cpan.org>
2013-05-18 23:49:54 +0000

    Import of TOBYINK/Test-RDF-DOAP-Version-0.009 from CPAN.
    gitpan-cpan-distribution: Test-RDF-DOAP-Version
    gitpan-cpan-version:      0.009
    gitpan-cpan-path:         TOBYINK/Test-RDF-DOAP-Version-0.009.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Test/RDF/DOAP/Version.pm
+++ lib/Test/RDF/DOAP/Version.pm
@@ -2,15 +2,15 @@ package Test::RDF::DOAP::Version;
 
 use 5.010;
 use strict;
-use constant { FALSE => 0, TRUE => 1 };
 use utf8;
 
 use RDF::Trine qw[iri variable literal blank statement];
 use RDF::TrineX::Parser::Pretdsl;
 use Test::More;
 use URI::Escape qw[uri_escape];
+use URI::file;
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 our @EXPORT  = qw(doap_version_ok);
 our $DOAP    = RDF::Trine::Namespace->new('http://usefulinc.com/ns/doap#');
 
@@ -19,22 +19,21 @@ use base qw[Exporter];
 sub doap_version_ok
 {
 	my ($dist, $module) = @_;
-	unless ($module)
-		{ ($module = $dist) =~ s{-}{::}g; }
-
-	eval "use $module;";
-	my $version  = $module->VERSION;	
+	($module = $dist) =~ s{-}{::}g unless $module;
+	
+	eval "use $module";
+	my $version  = $module->VERSION;
 	my $dist_uri = iri(sprintf('http://purl.org/NET/cpan-uri/dist/%s/project', uri_escape($dist)));
 	
 	my $model = RDF::Trine::Model->temporary_model;
-
+	
 	my $turtle = RDF::Trine::Parser->new('Turtle');
 	while (<meta/*.{ttl,turtle,nt}>)
 	{
 		my $iri = URI::file->new_abs($_);
 		$turtle->parse_file_into_model("$iri", $_, $model);
 	}
-
+	
 	my $rdfxml = RDF::Trine::Parser->new('RDFXML');
 	while (<meta/*.{rdf,rdfxml,rdfx}>)
 	{
@@ -52,30 +51,30 @@ sub doap_version_ok
 	my $pattern = RDF::Trine::Pattern->new(
 		statement($dist_uri, $DOAP->release, variable('v')),
 		statement(variable('v'), $DOAP->revision, literal($version, undef, 'http://www.w3.org/2001/XMLSchema#string')),
-		);
+	);
 	my $iter = $model->get_pattern($pattern);
 	while (my $result = $iter->next)
 	{
 		pass('doap_version_ok');
 		return 1;
 	}
-
+	
 	$pattern = RDF::Trine::Pattern->new(
 		statement($dist_uri, $DOAP->release, variable('v')),
 		statement(variable('v'), $DOAP->revision, literal($version//'0')),
-		);
+	);
 	$iter = $model->get_pattern($pattern);
 	while (my $result = $iter->next)
 	{
 		pass('doap_version_ok');
 		return 1;
 	}
-
+	
 	diag("${module}->VERSION = $version");
 	$pattern = RDF::Trine::Pattern->new(
 		statement($dist_uri, $DOAP->release, variable('v')),
 		statement(variable('v'), $DOAP->revision, variable('r')),
-                );
+	);
 	$iter = $model->get_pattern($pattern);
 	my $found = 0;
 	while (my $result = $iter->next)
@@ -84,12 +83,20 @@ sub doap_version_ok
 		$found++;
 	}
 	diag("Found no metadata for any versions.") unless $found;
-
+	
 	fail('doap_version_ok');
 	return 0;
 }
 
-TRUE;
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=for stopwords ver xsd:string
 
 =head1 NAME
 
@@ -127,7 +134,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2011-2012 by Toby Inkster.
+This software is copyright (c) 2011-2013 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
